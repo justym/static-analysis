e7f485bf4bd27400611d644041c44f15bad96202
justym <mm6mm0mm3mm@gmail.com>
2020-01-21 11:11:41 +0900

    change to dump-ast

--- .example/dump-ast/dump.go
+++ .example/dump-ast/dump.go
@@ -10,10 +10,17 @@ import (
 
 func main(){
 	fset := token.NewFileSet()
+	fset2 := token.NewFileSet()
 	expr,err := parser.ParseExprFrom(fset,"","v+1",0)
 	if err != nil {
 		log.Println(err)
 		os.Exit(1)
 	}
 	ast.Print(fset,expr)
+	expr2,err := parser.ParseExprFrom(fset2,"","dumper - 10",0)
+	if err != nil{
+		log.Println(err)
+		os.Exit(1)
+	}
+	ast.Print(fset2,expr2)
 }
